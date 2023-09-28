a795787527e0c3a84bdcc9f3fa23b64e73097a8f
özkan demir <ozkanndemir2009@gmail.com>
2023-09-28 23:30:09 +0300

    Navbar finished layout fixed

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,54 +1,46 @@
-"use client"
+"use client";
 import React from "react";
 import VideoComponent from "../Video";
 import MyButton from "../Button";
 import Image from "next/image";
 
-
 const Navbar = () => {
   return (
-    <div className="flex  p-44 justify-around   my-2  mx-2 h-full ">
-      <div>
-        <div className=" flex-col ">
+    <div className="flex flex-col lg:flex-row p-4 justify-center items-center lg:justify-around my-8 mx-3">
+      <div className="lg:mr-4">
+        <div className="flex-col text-center lg:text-left">
           <div
+            className="text-4xl lg:text-6xl font-semibold leading-tight"
             style={{
-              fontSize: "70px",
-              fontWeight: "700",
-              lineHeight: "80px",
-              textDecoration: "none solid rgb(255,255,255)",
-              height: "300px",
-              width: "545px",
+              margin: "20px",
+              lineHeight: "1.4",
             }}
           >
-            Ortalama Kripto <br /> Borsalarından Daha Hızlı,Daha İyi,Daha Güçlü
+            Ortalama Kripto <br />  Borsalarından  <br /> Daha Hızlı, Daha <br /> İyi, Daha Güçlü
           </div>
-          <div className="flex  items-center  flex-wrap justify-between  gap-20 mt-40">
-            <div className="border-white">
-              <input
-                className=" bg-black "
-                type="text"
-                placeholder="E-posta/Telefon"
-              />
-            </div>
-            <div>
-              <MyButton />
-            </div>
+          <div className="flex flex-col items-center lg:flex-row lg:items-center gap-2 mt-4">
+            <input
+              className="bg-black border-2 border-white px-8 py-9 rounded-md text-lg font-medium h-12 w-full lg:w-48 focus m-4"
+              type="text"
+              placeholder="E-posta/Telefon"
+            />
+            <MyButton className="mt-2 lg:mt-1" />
           </div>
-          <div className="flex  items-center flex-wrap justify-between gap-30 mt-40">
+          <div className="flex items-center flex-wrap justify-center lg:justify-between w-full  mt-4  ">
             <div>
               <Image
                 src="https://static.okx.com/cdn/assets/imgs/236/DD7AC9432E675714.png"
                 alt="Ortalama Kripto Borsalarından Daha Hızlı, Daha İyi, Daha Güçlü"
-                width={165}
-                height={120}
+                width={120}
+                height={88}
               />
             </div>
             <div>
               <Image
                 src="https://static.okx.com/cdn/assets/imgs/2210/499A92F3657A52EC.png"
                 alt="Ortalama Kripto Borsalarından Daha Hızlı, Daha İyi, Daha Güçlü"
-                width={160}
-                height={120}
+                width={120}
+                height={88}
               />
             </div>
             <div>
@@ -62,7 +54,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div>
+      <div className="mt-4 lg:mt-0">
         <div>
           <VideoComponent />
         </div>
