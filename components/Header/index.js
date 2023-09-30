58dfbb1466a842b409845eb608cf70a9776d4bd5
özkan demir <ozkanndemir2009@gmail.com>
2023-09-30 16:44:20 +0300

    fixed

--- components/Header/index.js
+++ components/Header/index.js
@@ -46,12 +46,12 @@ const Header = () => {
         {/* Mobile nav */}
         <ul
           className={`
-        md:hidden bg-black absolute   w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        md:hidden bg-black absolute   w-full top-0 overflow-y-auto bottom-0 py-20 pl-4
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
         >
           <li>
-            <Link href="/" className="py-7 px-3 inline-block text-black ">
+            <Link href="/" className="py-7 px-2 inline-block text-black ">
               Home
             </Link>
           </li>
